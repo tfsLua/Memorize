2ee9c4d90a4d2a7fbdd016a13071e48a58c9f0d6
Priscila Maciel de Lima <164570930+PriscilaMdeLima@users.noreply.github.com>
2025-05-07 13:24:59 -0300

    Add files via upload

---
+++ CLI/cliinput.h
@@ -0,0 +1,12 @@
+#ifndef CLIINPUT_H
+#define CLIINPUT_H
+
+#include <stdio.h>
+
+static void waitForEnter() {
+    printf("\nPressione Enter para continuar...");
+    getchar();
+    getchar();
+}
+
+#endif
